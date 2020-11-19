5f85a6572c4197d564ae7e0c9d0cd62b1bee37f7
i1620 <68018020+i1620@users.noreply.github.com>
2020-11-18 18:52:15 -0600

    made changes to state app

--- state_app.js
+++ state_app.js
@@ -223,6 +223,20 @@ function init() {
 });
 }
 
-
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+d3.select("select")
+    .selectAll("option")
+    .data(states)
+    .text(function (d) {
+        return d
+    })
+d3.select("select")
+    .selectAll("option")
+    .data(states)
+    .enter()
+    .append("option")
+    .text(function (d) {
+        return d
+    })
 
 init();
