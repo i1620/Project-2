6a38b94fa22b52094d711d177fd75ec2f6e5279d
Jamison Unruh <jamisonunruh24@gmail.com>
2020-11-21 13:23:18 -0600

    rewrote state.js so data could be selected by state

--- state_app.js
+++ state_app.js
@@ -1,242 +1,132 @@
-// function getPlot(id) {
+d3.csv("data_sources/daily_up 2.csv").then(function (data) {
+    var states = []
+    var positives = []
+    var deaths = []
+    var hospital = []
+    var date = []
+    var abbr = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
-//     d3.json("data_sources/national_data.json").then((data) => {
-//         console.log(data)
+    d3.select("select")
+        .selectAll("option")
+        .data(abbr)
+        .enter()
+        .append("option")
+        .text(function(d) {
+            return d
+        })
 
-//         var trace1 = {
-//             x: "date",
-//             y: "deaths",
-//             type: 'scatter'
-//         };
+// You can also define an anonymous function inline
+    for (var i = 0; i < data.length; i++) {
+        states.push(data[i].state);
+        positives.push(data[i].positive);
+        deaths.push(data[i].death);
+        hospital.push(data[i].hospitalizedCurrently);
+        date.push(data[i].date);
+    }
+    year = date[0][0] + date[0][1] + date[0][2] + date[0][3];
+    month = date[0][4] + date[0][5];
+    day = date[0][6] + date[0][7]
+    console.log(month + "-" +  day + "-" + year)
 
-//         var trace2 = {
-//             x: "date",
-//             y: "samples.positive",
-//             type: 'scatter'
-//         };
+    var dateformated = []
+    for (var i = 0; i < date.length; i++) {
+        year = date[i][0] + date[i][1] + date[i][2] + date[i][3];
+        month = date[i][4] + date[i][5];
+        day = date[i][6] + date[i][7]
+        dateformated.push(month + "-" +  day + "-" + year)
+    }
 
-//         var data = [trace1, trace2];
+    console.log(dateformated)
+    // initialize all data for alabama into array to be used for graphs
+    function init() {
+        var alPositives = []
+        var alDeaths = []
+        var alHospital = []
+        var alDate = []
+        for (var i = 0; i < states.length; i++) {
+            if (states[i] === "AL") {
+                alPositives.push(positives[i]);
+                alDeaths.push(deaths[i]);
+                alHospital.push(hospital[i]);
+                alDate.push(dateformated[i]);
+            }
+        }
+        console.log(alDate)
+        var trace1 = {
+            x: alDate.reverse(),
+            y: alDeaths.reverse(),
+            name: "Deaths",
+            type: "line"
+        }; 
 
-//         Plotly.newPlot('myDiv', data);
+        var trace2 = {
+            x: alDate.reverse(),
+            y: alHospital.reverse(),
+            name: "Hospitalized Cumulative",
+            type: "line"
+        };
 
+        var trace3 = {
+            x: alDate.reverse(),
+            y: alPositives.reverse(),
+            name: "Positive Results",
+            type: "line"
+        };
 
-//     });
-// }
+        
+        var chartData = [trace1,trace2,trace3];
 
-// function getInfo(id) {
+        var layout = {
+            xaxis:{
+                title:"Dates",
+                tickmode: "array",
+                tickvals: ["03-15-2020", "04-15-2020","05-15-2020","06-15-2020","07-15-2020","08-15-2020","09-15-2020","10-15-2020","11-15-2020"],
+                ticktext: ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November']
+            },
+            height: 600,
+            width: 1000
+        };
+        Plotly.newPlot('line', chartData, layout);
+    
+    }
 
-//     // Read Data
-//     d3.json("data_sources/national_data.json").then((data) => {
+              // Call updatePlotly() when a change takes place to the DOM
+            d3.selectAll("#selDataset").on("change", updatePlotly);
+          
+              // This function is called when a dropdown menu item is selected
+            function updatePlotly() {
+                // Use D3 to select the dropdown menu
+                var dropdownMenu = d3.select("#selDataset");
+                // Assign the value of the dropdown menu option to a variable
+                var dataset = dropdownMenu.property("value");
+              
+                // // Initialize x and y arrays
+                // var x = [];
+                // var y = [];
+                // var hover = [];
+                var statePositives = []
+                var stateDeaths = []
+                var stateHospital = []
+                var stateDate = []
+                for (var i = 0; i <states.length; i++) {
+                    if (dataset === states[i])
+                        statePositives.push(positives[i]),
+                        stateDeaths.push(deaths[i]),
+                        stateHospital.push(hospital[i]),
+                        stateDate.push(dateformated[i]);
 
-//         var date = data.date;
-//         console.log(date)
+                }
+                var update = {
+                    x: [stateDate.reverse(), stateDate.reverse(), stateDate.reverse()],
+                    y: [stateDeaths.reverse(), stateHospital.reverse(), statePositives.reverse()]
+                    }
 
-//         var result = date.filter(meta => meta.id.toString() == id)[0]
-
-//         var date_info = d3.select("#sample-metadata");
-
-//         date_info.html("");
-
-//         Object.entries(result).forEach((key) => {
-//             date_info.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
-//         });
-//     });
-// }
-
-// function init() {
-
-//     var dropdown = d3.select("#selDataset");
-
-//     // Read Data
-//     d3.json("data_sources/national_data.json").then((data) => {
-//         console.log(data)
-
-//         data.date.forEach(function(date) {
-//             dropdown.append("option").text(date).property("value");
-//         });
-
-//         getPlot(data.date[0]);
-//         getInfo(data.date[0]);
-//     });
-// }
-
-// function optionChanged(id) {
-//     init();
-//     getInfo(id);
-// }
-
-// init();
-
-// function init() {
-
-//     d3.json("data_sources/daily_up.json").then((importedData) => {   
-//         console.log(importedData)
-//         var data = importedData;
-
-//         // data.sort(function (a, b) {
-//         //     return parseFloat(b.date) - parseFloat(a.date);
-
-//         // });
-
-//         // data = data.slice(0, 100);
-//         // data = data.reverse();
-
-//         // var date1 = GetFormattedDate(data.map(row => row.date))
-//         console.log(data.dateModified)
-
-//         data.dateModified.forEach(function (date) {
-//             dropdown.append("option").text(date).property("value");
-//         });
-
-//         var trace1 = {
-//             x: data.dateModified,
-//             y: data.death,
-//             name: "Deaths",
-//             type: "bar"
-//         };
-
-//         var trace2 = {
-//             x: data.dateModified,
-//             y: data.hospitalizedCumulative,
-//             name: "Hospitalized Cumulative",
-//             type: "bar"
-//         };
-
-//         var trace3 = {
-//             x: data.dateModified,
-//             y: data.positive,
-//             name: "Positive Results",
-//             type: "bar"
-//         };
-
-
-//         var chartData = [trace1, trace2, trace3];
-
-//         var layout = {
-//             xaxis: { title: "Dates" },
-//             height: 600,
-//             width: 1000
-//         };
-//         Plotly.newPlot('scatter', chartData, layout);
-//     });
-// }
-
-// function GetFormattedDate(date) {
-//         var todayTime = new Date(date);
-//         // var month = format(todayTime .getMonth() + 1);
-//         // var day = format(todayTime .getDate());
-//         // var year = format(todayTime .getFullYear());
-//         // return month + "/" + day + "/" + year;
-//         return todayTime;
-//     }
-
-// function init() {
-
-//     d3.json("data_sources/daily_up.json").then((importedData) => {
-//         console.log(importedData)
-//         var data = importedData;
-
-//         // data.sort(function (a, b) {
-//         //     return parseFloat(b.date) - parseFloat(a.date);
-//         // });
-
-//         // data = data.slice(0, 100);
-//         // data = data.reverse();
-
-//         var trace1 = {
-//             x: data.date_modified,
-//             y: data.death,
-//             name: "Deaths",
-//             type: "bar"
-//         };
-
-//         var trace2 = {
-//             x: data.date_modified,
-//             y: data.hospitalizedCumulative,
-//             name: "Hospitalized Cumulative",
-//             type: "bar"
-//         };
-
-//         var trace3 = {
-//             x: data.date_modified,
-//             y: data.positive,
-//             name: "Positive Results",
-//             type: "bar"
-//         };
+                Plotly.restyle("line", update);
+                        
 
 
-//         var chartData = [trace1, trace2, trace3];
-
-//         var layout = {
-//             xaxis: { title: "Dates" },
-//             height: 600,
-//             width: 1000
-//         };
-//         Plotly.newPlot('line', chartData, layout);
-//     });
-// }
+                }
 
 
-function init() {
-
-    d3.csv("data_sources/daily_up.csv").then(function (data) {
-        console.log(data[0]);
-
-    data.sort(function(a,b){
-        return parseFloat(b.date) - parseFloat(a.date);
-
-    });
-
-    // data = data.slice(0,100);
-    data = data.reverse();  
-
-    var trace1 = {
-        x: data.map(row => row.date_modified),
-        y: data.map(row => row.death),
-        name: "Deaths",
-        type: "line"
-    };
-
-    var trace2 = {
-        x: data.map(row => row.date_modified),
-        y: data.map(row => row.hospitalizedCumulative),
-        name: "Hospitalized Cumulative",
-        type: "line"
-    };
-
-    var trace3 = {
-        x: data.map(row => row.date_modified),
-        y: data.map(row => row.positive),
-        name: "Positive Results",
-        type: "line"
-    };
-
-
-    var chartData = [trace1,trace2,trace3];
-
-    var layout = {
-        xaxis:{title:"Dates"},
-        height: 600,
-        width: 1000
-    };
-    Plotly.newPlot('line', chartData, layout);
-});
-}
-
-var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-d3.select("select")
-    .selectAll("option")
-    .data(states)
-    .text(function (d) {
-        return d
-    })
-d3.select("select")
-    .selectAll("option")
-    .data(states)
-    .enter()
-    .append("option")
-    .text(function (d) {
-        return d
-    })
-
-init();
+    init();
+})
